7125d85bf352baf409714356e1c9bb15f57df912
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-11-21 21:57:03 +0100

    Add gradle project files

---
+++ desktop/src/com/lop/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lop.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lop.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
