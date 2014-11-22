cce776b9fc9c57e14f1815832e58bda3ec10833e
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-11-22 22:52:14 +0100

    Rname bush variable

--- core/src/com/lop/game/Ground.java
+++ core/src/com/lop/game/Ground.java
@@ -6,13 +6,13 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class Ground implements Renderable{
 	private Sprite[] groundSprites = new Sprite[21];
-	private Sprite grassSprite;
-	private Sprite bigGrassSprite;
+	private Sprite littleBushSprite;
+	private Sprite bigBushSprite;
 
     public Ground(TextureAtlas spriteAtlas) {
         Sprite groundSprite = spriteAtlas.createSprite("slice03");
-        grassSprite = spriteAtlas.createSprite("little_bush");
-        bigGrassSprite = spriteAtlas.createSprite("big_bush");
+        littleBushSprite = spriteAtlas.createSprite("little_bush");
+        bigBushSprite = spriteAtlas.createSprite("big_bush");
 
         for (int i = 0; i < groundSprites.length; i++) {
             groundSprites[i] = groundSprite;
@@ -23,12 +23,12 @@ public class Ground implements Renderable{
         for (int i = 0; i < groundSprites.length; i++) {
             spriteBatch.draw(groundSprites[i], -21 + (2 * i), 0, 2f, 2f);
         }
-        spriteBatch.draw(grassSprite, -21 + 3, 2, 2f, 2f);
-        spriteBatch.draw(grassSprite, -21 + 6, 2, 2f, 2f);
-        spriteBatch.draw(grassSprite, -21 + 11, 2, 2f, 2f);
-        spriteBatch.draw(bigGrassSprite, -21 + 18, 2, 6f, 2f);
-        spriteBatch.draw(grassSprite, -21 + 22, 2, 2f, 2f);
-        spriteBatch.draw(grassSprite, -21 + 25, 2, 2f, 2f);
-        spriteBatch.draw(grassSprite, -21 + 33, 2, 2f, 2f);
+        spriteBatch.draw(littleBushSprite, -21 + 3, 2, 2f, 2f);
+        spriteBatch.draw(littleBushSprite, -21 + 6, 2, 2f, 2f);
+        spriteBatch.draw(littleBushSprite, -21 + 11, 2, 2f, 2f);
+        spriteBatch.draw(bigBushSprite, -21 + 18, 2, 6f, 2f);
+        spriteBatch.draw(littleBushSprite, -21 + 22, 2, 2f, 2f);
+        spriteBatch.draw(littleBushSprite, -21 + 25, 2, 2f, 2f);
+        spriteBatch.draw(littleBushSprite, -21 + 33, 2, 2f, 2f);
     }
 }
