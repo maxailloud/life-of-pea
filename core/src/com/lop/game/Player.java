6c1302f6aaaaa36fea339f991fa9ead467e83344
oramy <ymarua@hotmail.fr>
2014-11-22 11:21:05 +0100

    Renderable added.

--- core/src/com/lop/game/Player.java
+++ core/src/com/lop/game/Player.java
@@ -1,10 +1,13 @@
 package com.lop.game;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Shape;
 
-public class Player {
+public class Player implements Renderable{
 	private MyGame game;
 	private Sprite sprite;
 	private int rank;
@@ -35,6 +38,13 @@ public class Player {
 	}
 	public int getJumpCollisions() {
 		return jumpCollisions;
+	}
+	@Override
+	public void render(SpriteBatch batch) {
+		for(Fixture fix : body.getFixtureList()){
+			Shape shape = fix.getShape();
+			game.batch.draw(getSprite(), body.getPosition().x, body.getPosition().y, shape.getRadius() * 2, shape.getRadius() * 2);
+		}
 	}
 
 }
