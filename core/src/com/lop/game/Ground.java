2068088229b7e6bf2f77db75f932f4b719e86f06
Maxime AILLOUD <maxime.ailloud@gmail.com>
2014-11-22 22:50:35 +0100

    Add grass on the ground

--- core/src/com/lop/game/Ground.java
+++ core/src/com/lop/game/Ground.java
@@ -6,12 +6,16 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 public class Ground implements Renderable{
 	private Sprite[] groundSprites = new Sprite[21];
+	private Sprite grassSprite;
+	private Sprite bigGrassSprite;
 
     public Ground(TextureAtlas spriteAtlas) {
-        Sprite sprite = spriteAtlas.createSprite("slice03");
+        Sprite groundSprite = spriteAtlas.createSprite("slice03");
+        grassSprite = spriteAtlas.createSprite("little_bush");
+        bigGrassSprite = spriteAtlas.createSprite("big_bush");
 
         for (int i = 0; i < groundSprites.length; i++) {
-            groundSprites[i] = sprite;
+            groundSprites[i] = groundSprite;
         }
     }
 
@@ -19,5 +23,12 @@ public class Ground implements Renderable{
         for (int i = 0; i < groundSprites.length; i++) {
             spriteBatch.draw(groundSprites[i], -21 + (2 * i), 0, 2f, 2f);
         }
+        spriteBatch.draw(grassSprite, -21 + 3, 2, 2f, 2f);
+        spriteBatch.draw(grassSprite, -21 + 6, 2, 2f, 2f);
+        spriteBatch.draw(grassSprite, -21 + 11, 2, 2f, 2f);
+        spriteBatch.draw(bigGrassSprite, -21 + 18, 2, 6f, 2f);
+        spriteBatch.draw(grassSprite, -21 + 22, 2, 2f, 2f);
+        spriteBatch.draw(grassSprite, -21 + 25, 2, 2f, 2f);
+        spriteBatch.draw(grassSprite, -21 + 33, 2, 2f, 2f);
     }
 }
